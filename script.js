54a46aefbf133bce6e55d157a008a8156d9b0dfa
hmhtom <hmhtom@gmail.com>
2022-09-13 17:48:26 -0400

    Initialization of values in js
    assigning initial values to criteria form, fetch ok button element and form element, assigning correspondent char-sets to constant

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var okBtn = document.querySelector("#ok");
+var criteria = document.querySelector("#criteria")
+criteria.style.display="none";
 
+const LowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const UpperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const Numbers = "0123456789"
+const SpecialCharacters = String.raw ` !"#$%&'()*+,-./:;<=>?@[\]^_{|}~`+'`';
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
