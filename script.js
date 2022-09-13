4259b58f92e3bbb79c5b602f7a94caf5c836d231
hmhtom <hmhtom@gmail.com>
2022-09-13 17:51:06 -0400

    password generator
    add password generator function base on the form input value

--- script.js
+++ script.js
@@ -13,8 +13,36 @@ const SpecialCharacters = String.raw ` !"#$%&'()*+,-./:;<=>?@[\]^_{|}~`+'`';
 function criteriaShow(){
   criteria.style.display="flex";
 }
+//Password Generator
+function generatePassword() {
+  //Get the value from the form input
+  let length = document.getElementById("length").value;
+  let lowercaseletter = document.getElementById("lowercase").checked;
+  let uppercaseletter = document.getElementById("uppercase").checked;
+  let numbers = document.getElementById("numbers").checked;
+  let special = document.getElementById("special").checked;
+  //Initializing char_set to choose from and password to be generated
+  let char_set = "";
+  let password = "";
+  //Adding sets of char into char_set base on the checkbox value
+  if(lowercaseletter){char_set += LowerCaseLetters;}
+  if(uppercaseletter){char_set += UpperCaseLetters;}
+  if(numbers){char_set += Numbers;}
+  if(special){char_set += SpecialCharacters;}
+  //Loop over the length of password
+  for(let i = 0; i < length;i++){
+    let ranindex = getRandint(0, char_set.length-1);  //Getting a random index pointer for char_set
+    password += char_set.charAt(ranindex);  //Apending char of random index to password
+  }
+  return password;
+}
+//Get a random integer from min to max
+function getRandint(min, max) {
+  return Math.floor(Math.random()*(max - min))+min;
+}
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
